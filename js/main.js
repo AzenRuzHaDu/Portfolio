d002dc4e362d79ad39c6e046e87d5103f5032599
Madiane <Madiane@mad.com>
2022-04-06 13:14:18 +0200

    Ajout de la front-end de base

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+const boutonGeriou = document.querySelector(".bouton_geriou");
+
+
+
+const searchCity = (ev) => {
+  ev.preventDefault(); //prevent submitting
+  let city = document.getElementById("input-geriou").value;
+  url = new URL("http://127.0.0.1:8000/" + city +'/')
+  fetch(url)
+    .then(function(res) {
+      return res.json()
+
+    })
+    .then(function(html) {
+      var contVilles = document.getElementById('infos-villes');
+      contVilles.classList.add('active');
+      var villes = html[0]['fields']
+      console.log(villes)
+      contVilles.innerHTML = villes['nom_br'];
+
+    })
+    document.getElementById('form-geriou').reset();
+
+}
+
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.getElementById('btn-geriou').addEventListener('click',searchCity);
+});
+
+// const flipCard = () => {
+//   var card = document.getElementById("card")
+//   card.classList.add('turn')
+// }
+
+const boutonFlipRight = document.querySelectorAll(".btn-turn-right")
+
+boutonFlipRight.forEach(item => {
+  item.addEventListener('click', event => {
+    const par = event.target.parentElement.parentElement.parentElement;
+    console.log ("couccou", par.className)
+    par.classList.toggle('turn');
+  })
+})
+
+const boutonFlipLeft = document.querySelectorAll(".btn-turn-left")
+
+boutonFlipLeft.forEach(item => {
+  item.addEventListener('click', event => {
+    const par = event.target.parentElement.parentElement.parentElement;
+    console.log ("couccou", par.className)
+    par.classList.toggle('turn');
+  })
+})
+
+
+
+// .addEventListener('click', flipCard)
+
